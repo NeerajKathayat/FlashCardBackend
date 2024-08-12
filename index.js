96edee01b8f3aec8e0896bbe60ae67b9ea40d240
NeerajKathayat <98410996+NeerajKathayat@users.noreply.github.com>
2024-08-13 03:46:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 const corsOptions = {
-    origin: 'http://localhost:5174', // Change this to the origin of your frontend
+    origin: 'https://flash-card-beige.vercel.app', // Change this to the origin of your frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 };
